889b74cfdf37b8be0236dc76c616965b0c0ca1a3
aasnetoo <115680563+aasnetoo@users.noreply.github.com>
2022-11-23 19:03:47 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Produto produto = new Produto();
+        List<Produto> produtos = new ArrayList<>();
+        List<Produto> produtosFiltrados = new ArrayList<>();
+        boolean continuar = true;
+        while(continuar) {
+            Utils.mostrarMenu(scan, produto, produtos, produtosFiltrados);
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
